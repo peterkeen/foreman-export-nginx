449b5cabe916dde178b814531622f2951456ee02
Pete Keen <peter.keen@bugsplat.info>
2013-01-10 21:41:20 -0800

    Use ivars

--- lib/foreman/export/nginx.rb
+++ lib/foreman/export/nginx.rb
@@ -3,11 +3,24 @@ require 'foreman/export'
 module Foreman
   module Export
     class Nginx < Foreman::Export::Base
+
+      def env_var(key)
+        val = ENV[key]
+        val == "" ? nil : val
+      end
+
       def export
         super
 
         engine.each_process do |name, process|
           next unless name == 'web'
+
+          base_domain = env_var('BASE_DOMAIN') or raise "Need a BASE_DOMAIN"
+          additional_domains = (env_var('ADDITIONAL_DOMAINS') || '').split(',').join(' ')
+          use_ssl = !!env_var('USE_SSL')
+          ssl_cert_path = env_var('SSL_CERT_PATH')
+          ssl_key_path  = env_var('SSL_KEY_PATH')
+          force_ssl = !!env_var('FORCE_SSL')
 
           ports = []
           1.upto(engine.formation[name]) do |num|
